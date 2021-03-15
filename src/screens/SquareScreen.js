1d91319dfb7d563c03f12d65c335734ed800d291
Michael Stelly <bulletproofapps@pm.me>
2021-03-15 11:04:58 -0500

    removing ts config

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -7,11 +7,20 @@ const COLOR_INCREMENT = 15;
 const reducer = (state, action) => {
   switch (action.colorToChange) {
     case 'red':
-      return { ...state, red: state.red + action.amount };
+      return state.red + action.colorToChange > 255 ||
+        state.red + action.colorToChange < 0
+        ? state
+        : { ...state, red: state.red + action.amount };
     case 'green':
-      return { ...state, green: state.green + action.amount };
+      return state.green + action.colorToChange > 255 ||
+        state.green + action.colorToChange < 0
+        ? state
+        : { ...state, green: state.green + action.amount };
     case 'blue':
-      return { ...state, blue: state.blue + action.amount };
+      return state.blue + action.colorToChange > 255 ||
+        state.blue + action.colorToChange < 0
+        ? state
+        : { ...state, blue: state.blue + action.amount };
     default:
       return;
   }
